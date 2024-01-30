b827844a6f21f83f6540ffb44e20e1a128c93d81
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-01-30 17:05:01 +0100

    CREATE pages for doctors

---
+++ resources/views/admin/doctors/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Index doctors</h1>
+            </div>
+        </div>
+    </div>
+@endsection
