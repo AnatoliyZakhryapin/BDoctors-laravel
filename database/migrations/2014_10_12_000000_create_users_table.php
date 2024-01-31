917b81ed817dfb8ec30a5ddd673f9726159c1344
sabri <sabrinatwitter6@gmail.com>
2024-01-31 10:49:27 +0100

    fix user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,11 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('surname', 100);
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 40);
             $table->rememberToken();
             $table->timestamps();
         });
