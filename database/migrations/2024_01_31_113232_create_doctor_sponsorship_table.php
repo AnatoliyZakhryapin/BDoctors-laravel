c6123998785ea61d534a47d1cd84a39b19c7b199
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-02-01 09:32:08 +0100

    FIX function down of doctor_sponsorship

--- database/migrations/2024_01_31_113232_create_doctor_sponsorship_table.php
+++ database/migrations/2024_01_31_113232_create_doctor_sponsorship_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('doctor_sposorship');
+        Schema::dropIfExists('doctor_sponsorship');
     }
 };
