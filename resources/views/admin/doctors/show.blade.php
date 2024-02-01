bbad1b457f02dd043f8d028a045721a1785a926d
danieleardizzone <daniele.ardi02@gmail.com>
2024-02-01 11:01:19 +0100

    ADD - doctor show

--- resources/views/admin/doctors/show.blade.php
+++ resources/views/admin/doctors/show.blade.php
@@ -4,7 +4,16 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Show doctors</h1>
+                <h1>Dott. {{ $doctor->user->name }} {{ $doctor->user->surname }}</h1>
+                <img src="{{ $doctor->photo }}" alt="">
+                <p>{{ $doctor->address }}</p>
+                <p>{{ $doctor->phone_number }}</p>
+                <ul>
+                    @foreach ($doctor->specializations as $specialization)
+                        <li>{{ $specialization->name }}</li>
+                    @endforeach
+                </ul>
+                <p>{{ $doctor->medical_services }}</p>
             </div>
         </div>
     </div>
