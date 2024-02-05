bd6d49090af5ce58262542cb080b2080c7aafc32
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-02-05 10:02:44 +0100

    REFACTOR DASHBORD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>Welcome</h1>
+        <div class="row justify-content-center pt-5">
+            <div class="col-auto">
+                <h1>Benvenuto a BDoctors</h1>
+                <h3>Registra il tuo nuovo account o fai Login</h3>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\Admin\MessageController;
 use App\Http\Controllers\Admin\ReviewController;
 use App\Models\Message;
 use Illuminate\Support\Facades\Route;
+use App\Models\Doctor;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +22,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // Ottieni l'utente attualmente loggato
+    $logged_user = Auth::user();
+    // Recupera il dottore associato all'utente loggato.
+    // Restituisce un array di lunghezza 1 (relazione one-to-one)
+    $doctors = Doctor::where('user_id', '=', $logged_user->id)->get();
+    $doctor = $doctors[0];
+    return view('welcome', compact('doctor'));
 });
 
-Route::get('/dashboard', function () {
-    return view('admin/dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('admin/dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -36,6 +45,7 @@ Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(
     Route::resource('doctors', DoctorController::class);
     Route::resource('messages', MessageController::class);
     Route::resource('reviews', ReviewController::class);
+    Route::resource('dashboard', DashboardController::class);
 });
 
 require __DIR__ . '/auth.php';
